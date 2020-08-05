a171b7f61cf9c6dc7472399caf2075e9c130d7ad
Brigitta <bridgetlandia@gmail.com>
2020-08-05 16:32:55 +0200

    Compiling with Babel

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/orders', orderRoute);
 
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+	res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
 });
 
 app.listen(config.PORT, () => {
