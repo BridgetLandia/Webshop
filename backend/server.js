7a62f6264aa1401c677b09ede22fb66cbd2f8b55
Brigitta <bridgetlandia@gmail.com>
2020-08-05 16:20:59 +0200

    Changing package json

--- backend/server.js
+++ backend/server.js
@@ -21,21 +21,12 @@ app.use(bodyParser.json());
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/orders', orderRoute);
+
 app.use(express.static(path.join(__dirname, '/../frontend/build')));
+
 app.get('*', (req, res) => {
 	res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
-
-// app.get('/api/products/:id', (req, res) => {
-// 	const productId = req.params.id;
-// 	const product = data.products.find((x) => x._id === productId);
-// 	if (product) res.send(product);
-// 	else res.status(404).send({ msg: 'Product Not Found.' });
-// });
-
-// app.get('/api/products', (req, res) => {
-// 	res.send(data.products);
-// });
 
 app.listen(config.PORT, () => {
 	console.log('Server strated');
